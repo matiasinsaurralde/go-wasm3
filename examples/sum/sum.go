077fd2c0b04bf5e341652957b5c8a9926b5e1894
Matias Insaurralde <matias@insaurral.de>
2020-01-15 09:36:31 +0200

    Add "sum" example

---
+++ examples/sum/sum.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	wasm3 "github.com/matiasinsaurralde/go-wasm3"
+)
+
+const (
+	wasmFilename = "sum.wasm"
+	fnName       = "sum"
+)
+
+func main() {
+	log.Print("Initializing WASM3")
+
+	env := wasm3.NewEnvironment()
+	runtime := wasm3.NewRuntime(env, 64*1024)
+	log.Println("Runtime ok")
+
+	wasmBytes, err := ioutil.ReadFile(wasmFilename)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Read WASM module (%d bytes)\n", len(wasmBytes))
+
+	_, err = runtime.Load(wasmBytes)
+	if err != nil {
+		panic(err)
+	}
+	log.Print("Module loaded")
+
+	fn, err := runtime.FindFunction(fnName)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Calling function")
+	fn("1", "2")
+	fn("2", "2")
+}
